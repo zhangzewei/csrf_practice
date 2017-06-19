8ca3c1c6e11dc1716a2f0c499bf59c340aee743b
markzzw <453646474@qq.com>
2017-06-20 00:33:41 +0800

    feat: init csrf_practice project

---
+++ app.js
@@ -0,0 +1,10 @@
+var koa = require('koa');
+var app = new koa();
+
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.response.body = '<h1>Hello, koa2!</h1>';
+});
+
+app.listen(3000);
